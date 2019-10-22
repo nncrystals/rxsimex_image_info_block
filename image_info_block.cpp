4aca1f4c2ec227e7d0e5c53c255257a596f260af
Yuanyi Wu <wuyuanyi135@gmail.com>
2019-10-21 23:19:26 -0400

    using vector rather than multiple ports. Set high max to enable extensible function

--- image_info_block.cpp
+++ image_info_block.cpp
@@ -3,42 +3,37 @@
 using namespace simex;
 
 class image_info_block : public rx_remote_block {
- public:
-  explicit image_info_block(SimStruct* s) : rx_remote_block(s) {
+public:
+  explicit image_info_block(SimStruct *s) : rx_remote_block(s) {
     param_sample_time = register_scalar_parameter<double>("sample_time", false);
-    param_num_input = register_scalar_parameter<int>("num_input", false);
-    param_num_output = register_scalar_parameter<int>("num_output", false);
+    param_num_input = register_scalar_parameter<int>("max_num_input", false);
+    param_num_output = register_scalar_parameter<int>("max_num_output", false);
   }
 
- public:
+public:
   void on_parameter_updated() override {
     rx_remote_block::on_parameter_updated();
     sample_time.sample_time = param_sample_time->data;
     sample_time.offset_time = 0;
+    set_remote_accessible_port(register_input_port<double>(
+        "inputs", {param_num_input->data}, false, false, false));
 
-    for (int i = 0; i < param_num_input->data; ++i) {
-      auto ptr = register_input_port<double>(fmt::format("input{}", i), {1},
-                                             false, false, false);
-      set_remote_accessible_port(ptr);
-    }
-
-    for (int i = 0; i < param_num_output->data; ++i) {
-      auto ptr = register_output_port<double>(fmt::format("output{}", i), {1},
-                                              false, false);
-      set_remote_accessible_port(ptr);
-    }
+    set_remote_accessible_port(register_output_port<double>(
+        "outputs", {param_num_output->data}, false, false));
   }
   void on_start() override {
     rx_remote_block::on_start();
-    session_disconnected.get_observable().subscribe(subscriptions, [&](auto next) {
-      if (sessions.size() == 0) {
-        auto worker = rl->get_scheduler().create_worker(subscriptions);
-        auto stopper = rxcpp::schedulers::make_schedulable(worker, [](rxcpp::schedulers::schedulable self){
-          throw std::runtime_error("No client is connected. stop.");
+    session_disconnected.get_observable().subscribe(
+        subscriptions, [&](auto next) {
+          if (sessions.size() == 0) {
+            auto worker = rl->get_scheduler().create_worker(subscriptions);
+            auto stopper = rxcpp::schedulers::make_schedulable(
+                worker, [](rxcpp::schedulers::schedulable self) {
+                  throw std::runtime_error("No client is connected. stop.");
+                });
+            worker.schedule(stopper);
+          }
         });
-        worker.schedule(stopper);
-      }
-    });
 
     // wait for connection
     int retries = 10;
@@ -55,12 +50,12 @@ class image_info_block : public rx_remote_block {
     throw std::runtime_error("No client connected.");
   }
 
- private:
+private:
   std::shared_ptr<rx_scalar_param<double>> param_sample_time;
   std::shared_ptr<rx_scalar_param<int>> param_num_input;
   std::shared_ptr<rx_scalar_param<int>> param_num_output;
 };
 
-std::shared_ptr<rx_block> rx_block::create_block(SimStruct* S) {
+std::shared_ptr<rx_block> rx_block::create_block(SimStruct *S) {
   return std::make_shared<image_info_block>(S);
 }
